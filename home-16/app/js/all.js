094a21f258123356eb6f51b96a6878e5af5245ed
Dima Paschenko <pdv240222@i.ua>
2025-01-24 20:30:39 +0200

    home-16

--- home-16/app/js/all.js
+++ home-16/app/js/all.js
@@ -4027,6 +4027,16 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
 //     menu.classList.add("active");
 // });
 
+// $(".js-burger").on("click", function () {
+// 	if ($(this).hasClass('active')) {
+// 		 $(this).removeClass("active");
+// 		 $('.menu__list').removeClass('menu-show')
+// 	} else {
+// 		 $(this).addClass("active");
+// 		 $('.menu__list').addClass('menu-show')
+// 	}
+// });
+
 $('.js-slider').slick({
   infinite: true,
   slidesToShow: 1,
@@ -4065,4 +4075,42 @@ $('.js-slider').slick({
   // 	// settings: "unslick"
   // 	// instead of a settings object
   //  ]
+});
+$('.js-aboutslider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  // autoplay: true,
+  // autoplaySpeed: 1000,
+  arrows: false,
+  dots: true,
+  draggable: false,
+  mobileFirst: true,
+  responsive: [{
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      infinite: true,
+      dots: true
+    }
+  }
+  // 	{
+  // 	  breakpoint: 992,
+  // 	  settings: {
+  // 		 slidesToShow: 1,
+  // 		 slidesToScroll: 1
+  // 	  }
+  // 	},
+  // 	{
+  // 	  breakpoint: 480,
+  // 	  settings: {
+  // 		 slidesToShow: 1,
+  // 		 slidesToScroll: 1
+  // 	  }
+  // 	}
+  // 	// You can unslick at a given breakpoint now by adding:
+  // 	// settings: "unslick"
+  // 	// instead of a settings object
+  ]
 });
